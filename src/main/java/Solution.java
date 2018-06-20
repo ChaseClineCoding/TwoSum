9c7d117f8690b81307db5037f355835e3e7879ba
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-06-20 14:11:57 -0600

    finished solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+
 public class Solution
 {
     public static int[] twoSum(int[] numbers, int target)
     {
-
+        Integer[] nums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        int firstIndex = 0;
+        int secondIndex = 1;
+        while (firstIndex < numbers.length) {
+            secondIndex = Arrays.asList(nums).lastIndexOf((target - nums[firstIndex]));
+            if (secondIndex != -1 && secondIndex != firstIndex) break;
+            firstIndex++;
+        }
+        return new int[]{firstIndex, secondIndex};
     }
 }
